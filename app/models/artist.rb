83156842cff81655ba84de55bef5d54236d85c37
fadimata tillman <fadi.tillman@gmail.com>
2020-04-20 04:12:07 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,6 +3,8 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
+    self.songs
+  end
     #return the genre of the artist's first saved song
   end
 
